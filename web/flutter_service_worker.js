3e8c38bc034d67b8e9337d66a21ba3125cb7a5fc
Vatsal Jaganwala <vatsal.jaganwala@instanceit.com>
2025-06-25 15:30:53 +0530

    base changed

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fef771a8e93906c00757dfb96fd70fc1",
+const RESOURCES = {"flutter_bootstrap.js": "cc1064fb5671349a4d058e6e3c7a55e9",
 "extractTextWorker.js": "8a39ec744976d4c2a2fe44dc9a8ebade",
 "version.json": "a35b094700db26348ec2f2a53c39b005",
 "index.html": "92ab80eb94a09b944c0b2559312e0de5",
