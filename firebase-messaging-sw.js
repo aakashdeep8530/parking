937f77a2576575c8255298a817551a2638d322dd
Vatsal Jaganwala <vatsal.jaganwala@instanceit.com>
2025-07-03 17:36:45 +0530

    2025-07-03 17:36:44

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,43 @@
+self.addEventListener("push", e => {
+  console.log(JSON.stringify(e));
+  const tempData = e.data.json();
+  console.log(JSON.stringify(tempData));
+  const data = tempData.data
+  console.log(data);
+  self.registration.showNotification(
+    tempData.notification.title, // title of the notification
+    {
+      body: tempData.notification.body, //the body of the push notification
+      data: data,
+      icon: tempData.notification.icon,
+      badge: tempData.notification.badge,
+      vibrate: tempData.notification.vibrate,
+      sound: tempData.notification.sound,
+      actions: tempData.notification.actions,
+      tag: tempData.notification.tag,
+    }
+  );
+});
+
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data['alias'])
+  );
+})
+
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(preLoad());
+});
+
+let preLoad = function () {
+  return caches.open("offline").then(function (cache) {
+    return cache.addAll(["../src/index.js"]);
+  });
+};
+
+self.addEventListener("fetch", function (event) {
+
+});
